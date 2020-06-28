173ff88c3ace00c5e909f8db3d458e20370122fd
aniket-hue <freakyyo26@gmail.com>
2020-06-28 20:22:02 +0530

    Updates

---
+++ precache-manifest.034a0420a2776b6fcb3c1d669aae3e31.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d624e8d81eb3a274303598536c8d8ff",
+    "url": "/Madad-Demo/index.html"
+  },
+  {
+    "revision": "ecfc11a7ba51b3d6bddd",
+    "url": "/Madad-Demo/static/js/2.59462164.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Madad-Demo/static/js/2.59462164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002ecc6a2ea607913600",
+    "url": "/Madad-Demo/static/js/main.eb8916a0.chunk.js"
+  },
+  {
+    "revision": "2467f6c0f226b73ca603",
+    "url": "/Madad-Demo/static/js/runtime-main.28702acc.js"
+  },
+  {
+    "revision": "0a0789d909f41e1f01d5ee95c6fb2183",
+    "url": "/Madad-Demo/static/media/architect.0a0789d9.svg"
+  },
+  {
+    "revision": "1eb3b8db2a2c503325786f17cd573124",
+    "url": "/Madad-Demo/static/media/back.1eb3b8db.jpeg"
+  },
+  {
+    "revision": "c92475154d3b0486ce979acdf35bd6fc",
+    "url": "/Madad-Demo/static/media/banking.c9247515.svg"
+  },
+  {
+    "revision": "5a819b3dc888532ea7209af653d58d6c",
+    "url": "/Madad-Demo/static/media/cardbg.5a819b3d.svg"
+  },
+  {
+    "revision": "3c9fa23b8988924265b3a0c401d5cb82",
+    "url": "/Madad-Demo/static/media/consultant.3c9fa23b.svg"
+  },
+  {
+    "revision": "3e124644cd4ede5d498add8ef0cd22ed",
+    "url": "/Madad-Demo/static/media/cooking.3e124644.svg"
+  },
+  {
+    "revision": "b3af1d850c86b6103a15e6bfb976900d",
+    "url": "/Madad-Demo/static/media/demo_image.b3af1d85.svg"
+  },
+  {
+    "revision": "cb253e58350cf13b38a568143b23624d",
+    "url": "/Madad-Demo/static/media/depression.cb253e58.svg"
+  },
+  {
+    "revision": "2534f3d2b182fa511c0266c8f0e63fba",
+    "url": "/Madad-Demo/static/media/electrician.2534f3d2.svg"
+  },
+  {
+    "revision": "5410519d4837c5cd9875555457784217",
+    "url": "/Madad-Demo/static/media/experts.5410519d.svg"
+  },
+  {
+    "revision": "5a9a96a5c6ed1434ecde1362b0c13ff7",
+    "url": "/Madad-Demo/static/media/flights.5a9a96a5.svg"
+  },
+  {
+    "revision": "743aca9c055350d42d6d5044c92b70d0",
+    "url": "/Madad-Demo/static/media/hospital.743aca9c.svg"
+  },
+  {
+    "revision": "ba675f12dd978cbb9d69a054e9f7cfb4",
+    "url": "/Madad-Demo/static/media/medicine.ba675f12.svg"
+  },
+  {
+    "revision": "7fde02867e032666dc683438c9e73109",
+    "url": "/Madad-Demo/static/media/ngo.7fde0286.svg"
+  },
+  {
+    "revision": "6526ba182e8e8ab7a450e54ab7e2b6d7",
+    "url": "/Madad-Demo/static/media/trial.6526ba18.svg"
+  }
+]);
