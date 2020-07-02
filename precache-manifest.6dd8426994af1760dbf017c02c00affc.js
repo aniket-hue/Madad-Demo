8feb9474bbb40926216385c449aad3b4d001983c
aniket-hue <freakyyo26@gmail.com>
2020-07-02 10:24:19 +0530

    Updates

---
+++ precache-manifest.6dd8426994af1760dbf017c02c00affc.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d022c83617a7aa4adbbf7c9644855808",
+    "url": "/Madad-Demo/Home/index.html"
+  },
+  {
+    "revision": "0c3bc769db7789c2aa23",
+    "url": "/Madad-Demo/Home/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "4a99d5591a1c5d3110a9",
+    "url": "/Madad-Demo/Home/static/css/main.a7a3ec54.chunk.css"
+  },
+  {
+    "revision": "0c3bc769db7789c2aa23",
+    "url": "/Madad-Demo/Home/static/js/2.198222ab.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Madad-Demo/Home/static/js/2.198222ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a99d5591a1c5d3110a9",
+    "url": "/Madad-Demo/Home/static/js/main.ae2ea6ab.chunk.js"
+  },
+  {
+    "revision": "ce656f9f452e65d5618e",
+    "url": "/Madad-Demo/Home/static/js/runtime-main.06581a5b.js"
+  },
+  {
+    "revision": "0a0789d909f41e1f01d5ee95c6fb2183",
+    "url": "/Madad-Demo/Home/static/media/architect.0a0789d9.svg"
+  },
+  {
+    "revision": "c62e286693b807479ac1d6b26ca21a0e",
+    "url": "/Madad-Demo/Home/static/media/back.c62e2866.jpg"
+  },
+  {
+    "revision": "c92475154d3b0486ce979acdf35bd6fc",
+    "url": "/Madad-Demo/Home/static/media/banking.c9247515.svg"
+  },
+  {
+    "revision": "5a819b3dc888532ea7209af653d58d6c",
+    "url": "/Madad-Demo/Home/static/media/cardbg.5a819b3d.svg"
+  },
+  {
+    "revision": "3c9fa23b8988924265b3a0c401d5cb82",
+    "url": "/Madad-Demo/Home/static/media/consultant.3c9fa23b.svg"
+  },
+  {
+    "revision": "3e124644cd4ede5d498add8ef0cd22ed",
+    "url": "/Madad-Demo/Home/static/media/cooking.3e124644.svg"
+  },
+  {
+    "revision": "b3af1d850c86b6103a15e6bfb976900d",
+    "url": "/Madad-Demo/Home/static/media/demo_image.b3af1d85.svg"
+  },
+  {
+    "revision": "cb253e58350cf13b38a568143b23624d",
+    "url": "/Madad-Demo/Home/static/media/depression.cb253e58.svg"
+  },
+  {
+    "revision": "2534f3d2b182fa511c0266c8f0e63fba",
+    "url": "/Madad-Demo/Home/static/media/electrician.2534f3d2.svg"
+  },
+  {
+    "revision": "5410519d4837c5cd9875555457784217",
+    "url": "/Madad-Demo/Home/static/media/experts.5410519d.svg"
+  },
+  {
+    "revision": "7257aa522d63add4dda595b899157a67",
+    "url": "/Madad-Demo/Home/static/media/facebook.7257aa52.svg"
+  },
+  {
+    "revision": "5a9a96a5c6ed1434ecde1362b0c13ff7",
+    "url": "/Madad-Demo/Home/static/media/flights.5a9a96a5.svg"
+  },
+  {
+    "revision": "bb97157a072be24bdfddbd80513ee866",
+    "url": "/Madad-Demo/Home/static/media/harsh.bb97157a.jpg"
+  },
+  {
+    "revision": "4de39e283b72567dde012dab49512533",
+    "url": "/Madad-Demo/Home/static/media/helpline.4de39e28.svg"
+  },
+  {
+    "revision": "743aca9c055350d42d6d5044c92b70d0",
+    "url": "/Madad-Demo/Home/static/media/hospital.743aca9c.svg"
+  },
+  {
+    "revision": "0016fc47d02d9bda1922d9093833129e",
+    "url": "/Madad-Demo/Home/static/media/instagram.0016fc47.svg"
+  },
+  {
+    "revision": "ba675f12dd978cbb9d69a054e9f7cfb4",
+    "url": "/Madad-Demo/Home/static/media/medicine.ba675f12.svg"
+  },
+  {
+    "revision": "7fde02867e032666dc683438c9e73109",
+    "url": "/Madad-Demo/Home/static/media/ngo.7fde0286.svg"
+  },
+  {
+    "revision": "6526ba182e8e8ab7a450e54ab7e2b6d7",
+    "url": "/Madad-Demo/Home/static/media/trial.6526ba18.svg"
+  },
+  {
+    "revision": "c95cbd88c68e183c023ca430809dbbdb",
+    "url": "/Madad-Demo/Home/static/media/twitter.c95cbd88.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Madad-Demo/precache-manifest.a7ecce0346bbf55e711cdafa177ebfdc.js"
+  "/Madad-Demo/Home/precache-manifest.6dd8426994af1760dbf017c02c00affc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Madad-Demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Madad-Demo/Home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
